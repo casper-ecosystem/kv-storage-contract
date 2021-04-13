c694a70069ec15d95015bb24f1e19d57734c4530
home <home@homes-Mac-mini.local>
2021-04-13 12:35:23 +1000

    removed macro

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -2,8 +2,6 @@ extern crate alloc;
 use alloc::{collections::BTreeSet, string::String};
 use std::convert::TryInto;
 
-use casperlabs_contract_macro::{casperlabs_constructor, casperlabs_contract, casperlabs_method};
-
 use contract::{
     contract_api::{runtime, storage},
     unwrap_or_revert::UnwrapOrRevert,
@@ -13,50 +11,142 @@ use types::{
     account::AccountHash,
     bytesrepr::ToBytes,
     contracts::{EntryPoint, EntryPointAccess, EntryPointType, EntryPoints},
-    runtime_args, CLType, CLTyped, CLValue, Group, Parameter, RuntimeArgs, URef, U512,
+    runtime_args, CLType, CLTyped, Group, Parameter, RuntimeArgs, URef, U512,
 };
 
-#[casperlabs_contract]
-mod kvstorage_contract {
+#[no_mangle]
+pub extern "C" fn init() {}
 
-    #[casperlabs_constructor]
-    fn init() {}
+#[no_mangle]
+pub extern "C" fn store_u64() {
+    let name: String = runtime::get_named_arg("name");
+    let value: u64 = runtime::get_named_arg("value");
+    set_key(name.as_str(), value);
+}
 
-    #[casperlabs_method]
-    fn store_u64(name: String, value: u64) {
-        set_key(name.as_str(), value);
-    }
+#[no_mangle]
+pub extern "C" fn store_u512() {
+    let name: String = runtime::get_named_arg("name");
+    let value: U512 = runtime::get_named_arg("value");
+    set_key(name.as_str(), value);
+}
 
-    #[casperlabs_method]
-    fn store_u512(name: String, value: U512) {
-        set_key(name.as_str(), value);
-    }
+#[no_mangle]
+pub extern "C" fn store_string() {
+    let name: String = runtime::get_named_arg("name");
+    let value: String = runtime::get_named_arg("value");
+    set_key(name.as_str(), value);
+}
 
-    #[casperlabs_method]
-    fn store_string(name: String, value: String) {
-        set_key(name.as_str(), value);
-    }
+#[no_mangle]
+pub extern "C" fn store_account_hash() {
+    let name: String = runtime::get_named_arg("name");
+    let value: AccountHash = runtime::get_named_arg("value");
+    set_key(name.as_str(), value);
+}
 
-    #[casperlabs_method]
-    fn store_account_hash(name: String, value: AccountHash) {
-        set_key(name.as_str(), value);
-    }
+#[no_mangle]
+pub extern "C" fn store_bytes() {
+    let name: String = runtime::get_named_arg("name");
+    let value: Vec<u8> = runtime::get_named_arg("value");
+    set_key(name.as_str(), value);
+}
 
-    #[casperlabs_method]
-    fn store_bytes(name: String, value: Vec<u8>) {
-        set_key(name.as_str(), value);
-    }
+#[no_mangle]
+pub extern "C" fn call() {
+    let (contract_package_hash, _) = storage::create_contract_package_at_hash();
+    let _constructor_access_uref: URef = storage::create_contract_user_group(
+        contract_package_hash,
+        "constructor_group",
+        1,
+        BTreeSet::new(),
+    )
+    .unwrap_or_revert()
+    .pop()
+    .unwrap_or_revert();
+    let constructor_group = Group::new("constructor_group");
+    let mut entry_points = EntryPoints::new();
 
-    fn set_key<T: ToBytes + CLTyped>(name: &str, value: T) {
-        match runtime::get_key(name) {
-            Some(key) => {
-                let key_ref = key.try_into().unwrap_or_revert();
-                storage::write(key_ref, value);
-            }
-            None => {
-                let key = storage::new_uref(value).into();
-                runtime::put_key(name, key);
-            }
+    entry_points.add_entry_point(EntryPoint::new(
+        String::from("init"),
+        vec![],
+        CLType::Unit,
+        EntryPointAccess::Groups(vec![constructor_group]),
+        EntryPointType::Contract,
+    ));
+
+    entry_points.add_entry_point(EntryPoint::new(
+        String::from("store_u64"),
+        vec![
+            Parameter::new("name", CLType::String),
+            Parameter::new("value", CLType::U64),
+        ],
+        CLType::Unit,
+        EntryPointAccess::Public,
+        EntryPointType::Contract,
+    ));
+
+    entry_points.add_entry_point(EntryPoint::new(
+        String::from("store_u512"),
+        vec![
+            Parameter::new("name", CLType::String),
+            Parameter::new("value", CLType::U512),
+        ],
+        CLType::Unit,
+        EntryPointAccess::Public,
+        EntryPointType::Contract,
+    ));
+
+    entry_points.add_entry_point(EntryPoint::new(
+        String::from("store_string"),
+        vec![
+            Parameter::new("name", CLType::String),
+            Parameter::new("value", CLType::String),
+        ],
+        CLType::Unit,
+        EntryPointAccess::Public,
+        EntryPointType::Contract,
+    ));
+
+    entry_points.add_entry_point(EntryPoint::new(
+        String::from("store_account_hash"),
+        vec![
+            Parameter::new("name", CLType::String),
+            Parameter::new("value", AccountHash::cl_type()),
+        ],
+        CLType::Unit,
+        EntryPointAccess::Public,
+        EntryPointType::Contract,
+    ));
+
+    entry_points.add_entry_point(EntryPoint::new(
+        String::from("store_bytes"),
+        vec![
+            Parameter::new("name", CLType::String),
+            Parameter::new("value", CLType::List(Box::new(CLType::U8))),
+        ],
+        CLType::Unit,
+        EntryPointAccess::Public,
+        EntryPointType::Contract,
+    ));
+
+    let (contract_hash, _) =
+        storage::add_contract_version(contract_package_hash, entry_points, Default::default());
+    runtime::put_key("kvstorage_contract", contract_hash.into());
+    let contract_hash_pack = storage::new_uref(contract_hash);
+    runtime::put_key("kvstorage_contract_hash", contract_hash_pack.into());
+    runtime::call_contract::<()>(contract_hash, "init", runtime_args! {});
+}
+
+fn set_key<T: ToBytes + CLTyped>(name: &str, value: T) {
+    match runtime::get_key(name) {
+        Some(key) => {
+            let key_ref = key.try_into().unwrap_or_revert();
+            storage::write(key_ref, value);
+        }
+        None => {
+            let key = storage::new_uref(value).into();
+            runtime::put_key(name, key);
         }
     }
 }
